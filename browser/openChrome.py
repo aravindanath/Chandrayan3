b776e4ef11600bd72cdc025779a9cd5b7dd6a264
chreddyv <reddyvarichandra@gmail.com>
2019-09-14 10:31:58 +0530

    modified path to Windows and mac

--- browser/openChrome.py
+++ browser/openChrome.py
@@ -3,8 +3,18 @@ from selenium import  webdriver
 
 ops = webdriver.ChromeOptions()
 ops.add_argument("--disable-notifications")
-driver =  webdriver.Chrome(executable_path="../driver/chromedriver",options=ops)
-driver.fullscreen_window()
+env =  "win"
+
+if  env is "win":
+    path = "..//driver//chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=path, options=ops)
+    driver.fullscreen_window()
+elif env is "mac":
+    path = "..//driver//chromedriver"
+    driver = webdriver.Chrome(executable_path=path, options=ops)
+    driver.fullscreen_window()
+
+
 
 
 
