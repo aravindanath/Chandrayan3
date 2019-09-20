ee7fec550df6746ae6be35025a51d181f920dca7
aravindanath <aravindanath86@gmail.com>
2019-09-20 09:44:40 +0530

    Selenium builtins

---
+++ SeleniumBuiltins/Builtins.py
@@ -0,0 +1,23 @@
+import browser.openChrome as op
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+op.driver.get("https://www.google.com")
+print("Current url :  ",op.driver.current_url)
+op.driver.find_element_by_name("q").send_keys("Selenium jobs",Keys.ENTER)
+
+
+pageTitle = op.driver.title
+
+print("Page title is", pageTitle)
+time.sleep(2)
+op.driver.back()
+time.sleep(2)
+op.driver.forward()
+print("Current url :  ",op.driver.current_url)
+op.driver.refresh()
+time.sleep(2)
+print(op.driver.page_source)
+op.driver.quit()
+
