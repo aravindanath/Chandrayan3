6fa48ee0b86982446f8a9f421ce9f52fb4caafda
Martin <martinmpaul@yahoo.com>
2019-09-25 13:43:50 +0530

    Updated for printing the title of the frames

--- Assignment/framesAssignment-Martin.py
+++ Assignment/framesAssignment-Martin.py
@@ -6,38 +6,30 @@ url = "https://seleniumhq.github.io/selenium/docs/api/java/"
 
 op.driver.get(url)
 
+# Since the frames are within a frame, setting the focus to the main frame
 op.driver.switch_to.frame('packageListFrame')
-# Click on the first link under the default frame
+
+# Print title of the first frame
+print(op.driver.find_element_by_tag_name("title").get_attribute("innerHTML"))
+
+# Click on the first link under the first frame
 op.driver.find_element_by_xpath("//a[@href='com/thoughtworks/selenium/package-frame.html']").click()
 
-# print the title of the default frame
-# title = op.driver.find_element_by_xpath("//frame[@title='All Packages']").text
-# print("Title of the first frame is: ",title)
-
-# Switch focus to second frame
-#op.driver.switch_to_frame('packageFrame')
-
-# secondFrame = op.driver.find_element_by_xpath("//frame[@name='packageFrame']")
-# op.driver.switch_to_frame(secondFrame)
-#
-# frameTwo = op.driver.find_element_by_tag_name('frame')[1]
-# op.driver.switch_to_frame('packageFrame')
-# op.driver.switch_to_frame(frameTwo)
+# Switch focus to main window and then to second frame
 op.driver.switch_to.default_content()
+op.driver.switch_to.frame('packageFrame')
 
-op.driver.switch_to.frame("packageFrame")
+# print the title of the second frame
+print(op.driver.find_element_by_tag_name("title").get_attribute("innerHTML"))
+
 # Click on the first link under second frame
 op.driver.find_element_by_xpath("//span[@class='interfaceName' and contains(text(),'CommandProcessor')]").click()
 
-# print the title of the second frame
-# title = op.driver.find_element_by_xpath("//frame[@name='packageFrame']").text
-# print("Title of the second frame is: ",title)
+# Switch focus main window and then to third frame
 op.driver.switch_to.default_content()
-op.driver.switch_to.frame("classFrame")
-op.driver.find_element_by_xpath("//a[contains(@href,'HttpCommandProcessor.html')]").click()
+op.driver.switch_to.frame('classFrame')
 
-# print the title of the forth frame
-# title = op.driver.find_element_by_xpath("//frame[contains(@title, 'Package, class and')]").text
-# print("Title of the third frame is: ",title)
+# print the title of the third frame
+print(op.driver.find_element_by_tag_name("title").get_attribute("innerHTML"))
 
 op.driver.quit()
